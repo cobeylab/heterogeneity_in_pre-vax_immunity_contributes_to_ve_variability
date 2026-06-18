96f14f678aa211d95f99b52768d78317ce09b613
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2026-06-18 09:03:18 -0500

    adding code for supplemental fig re: ve model comparison

---
+++ scripts/supplemental_figures/ve_models_sensitivity_to_pre-vax_means.R
@@ -0,0 +1,278 @@
+library(here)
+library(tidyverse)
+library(survival)
+library(cowplot)
+library(paletteer)
+
+source(here("src", "full_VE_model.R"))
+source(here("src", "data_generation.R"))
+
+facet_labels = c(
+    insnt_ve = "VE instantaneous",
+    ulreg_ve = "VE unconditional",
+    clreg_ve = "VE conditional"
+)
+
+### final ve sweeping across lambda, epsilon, theta_0 (NO WANING)
+
+set.seed(0)
+
+pars = list(
+    start_time = 0,
+    end_time = 200,
+    dt = 14,
+    lambda = c(0.0015, 0.005, 0.01),
+    lambda_negative = 0.02,
+    theta_0 = 1 - seq(0.1, 0.9, 0.1),
+    eta = -1,
+    epsilon_v = seq(0.1, 1.0, 0.1),
+    epsilon_u = seq(0.1, 1.0, 0.1),
+    alpha_v = 20,
+    alpha_u = 20,
+    mu_v = 1,
+    mu_u = 1,
+    pi_pos = 1,
+    pi_neg = 1,
+    c = 0.5,
+    N = 1e3
+)
+
+pars$time <- pars$end_time
+
+opts = list(
+    waning = FALSE,
+    heterogeneity = TRUE,
+    instantaneous = FALSE
+)
+
+cumul_ve_opts <- opts
+insnt_ve_opts <- opts
+insnt_ve_opts$instantaneous <- TRUE
+
+pars_dt <- crossing(!!!pars) %>%
+    mutate(par_set_id = row_number()) %>%
+    group_by(par_set_id) %>%
+    nest(pars = -group_cols()) %>%
+    ungroup()
+
+print(paste0("generating ", nrow(pars_dt)," no waning scenario linelists"))
+
+final_ve_dt <- pars_dt %>%
+    rowwise_mutate(counts = list(generate_counts(pars, opts))) %>%
+    mutate(linelist = map(counts, function(x) generate_linelist(x))) %>%
+    mutate(
+        insnt_ve = map_vec(pars, function(x) estimate_math_ve(x, insnt_ve_opts)),
+        cumul_ve = map_vec(pars, function(x) estimate_math_ve(x, cumul_ve_opts)),
+        ulreg_ve = map_vec(linelist, function(x) estimate_ve_uncond_logreg(x)),
+        clreg_ve = map_vec(linelist, function(x) estimate_ve_cond_logreg(x))
+    ) %>%
+    unnest(pars) %>%
+    mutate(mean_ratio = epsilon_v / epsilon_u)
+
+print("generating no waning scenario plot")
+
+no_waning_insnt_vs_cumul_lm <- lm(insnt_ve ~ cumul_ve, data = final_ve_dt)
+no_waning_ulreg_vs_cumul_lm <- lm(ulreg_ve ~ cumul_ve, data = final_ve_dt)
+no_waning_clreg_vs_cumul_lm <- lm(clreg_ve ~ cumul_ve, data = final_ve_dt)
+
+no_waning_insnt_vs_cumul_adj_rsq <- summary(no_waning_insnt_vs_cumul_lm)$adj.r.squared
+no_waning_ulreg_vs_cumul_adj_rsq <- summary(no_waning_ulreg_vs_cumul_lm)$adj.r.squared
+no_waning_clreg_vs_cumul_adj_rsq <- summary(no_waning_clreg_vs_cumul_lm)$adj.r.squared
+
+no_waning_ve_compare <- final_ve_dt %>%
+    select(ends_with("_ve")) %>%
+    pivot_longer(!cumul_ve) %>%
+    group_by(name) %>%
+    summarize(mean_diff = mean(abs(value - cumul_ve))) %>%
+    ungroup() %>%
+    arrange(name) %>%
+    mutate(adj_rsq = c(no_waning_clreg_vs_cumul_adj_rsq,
+                       no_waning_insnt_vs_cumul_adj_rsq,
+                       no_waning_ulreg_vs_cumul_adj_rsq))
+
+no_waning_plt <- final_ve_dt %>%
+    pivot_longer(c(insnt_ve, ulreg_ve, clreg_ve)) %>%
+    ggplot() +
+        aes(x = cumul_ve, y = value, color = mean_ratio) +
+        geom_hline(yintercept = 0, linetype = "44", color = "gray50") +
+        geom_vline(xintercept = 0, linetype = "44", color = "gray50") +
+        geom_line(aes(y = cumul_ve), color = "black", linewidth = 1, linetype = "44") +
+        geom_point(size = 2) +
+        geom_smooth(
+            method = "lm",
+            linewidth = 2,
+            color = "#ff9f9faa"
+        ) +
+        geom_text(
+            data = no_waning_ve_compare,
+            aes(
+                x = -475,
+                y = 100,
+                label = paste0("Adj. r-squared: ", signif(adj_rsq, digits = 3))
+            ),
+            inherit.aes = FALSE
+        ) +
+        geom_text(
+            data = no_waning_ve_compare,
+            aes(
+                x = -475,
+                y = 85,
+                label = paste0("Mean abs. difference: ", signif(mean_diff, digits = 3))
+            ),
+            inherit.aes = FALSE
+        ) +
+        facet_grid(cols = vars(name)) +
+        scale_color_viridis_c(
+            name = "Mean pre-vaccintion susceptibility ratio",
+            limits = c(0.1, 10),
+            breaks = c(0.1, 10)
+        ) +
+        theme_cowplot(20) +
+        background_grid() +
+        theme(legend.position = "top") +
+        labs(x = "VE cumulative (%)", y = "Alternative VE estimate (%)")
+
+rm(pars_dt, final_ve_dt)
+
+### final ve sweeping across lambda, epsilon, theta_0, eta (WITH WANING)
+
+set.seed(0)
+
+pars = list(
+    start_time = 0,
+    end_time = 200,
+    dt = 14,
+    lambda = c(0.0015, 0.005, 0.01),
+    lambda_negative = 0.02,
+    theta_0 = 1 - c(0.3, 0.6, 0.9),
+    eta = c(30, 180, 360, 1440),
+    epsilon_v = seq(0.1, 1.0, 0.1),
+    epsilon_u = seq(0.1, 1.0, 0.1),
+    alpha_v = 20,
+    alpha_u = 20,
+    mu_v = 1,
+    mu_u = 1,
+    pi_pos = 1,
+    pi_neg = 1,
+    c = 0.5,
+    N = 1e3
+)
+
+pars$time <- pars$end_time
+
+opts = list(
+    waning = TRUE,
+    heterogeneity = TRUE,
+    instantaneous = FALSE
+)
+
+cumul_ve_opts <- opts
+insnt_ve_opts <- opts
+insnt_ve_opts$instantaneous <- TRUE
+
+pars_dt <- crossing(!!!pars) %>%
+    mutate(par_set_id = row_number()) %>%
+    group_by(par_set_id) %>%
+    nest(pars = -group_cols()) %>%
+    ungroup()
+
+print(paste0("generating ", nrow(pars_dt)," waning scenario linelists"))
+
+final_ve_dt <- pars_dt %>%
+    rowwise_mutate(counts = list(generate_counts(pars, opts))) %>%
+    mutate(linelist = map(counts, function(x) generate_linelist(x))) %>%
+    mutate(
+        insnt_ve = map_vec(pars, function(x) estimate_math_ve(x, insnt_ve_opts)),
+        cumul_ve = map_vec(pars, function(x) estimate_math_ve(x, cumul_ve_opts)),
+        ulreg_ve = map_vec(linelist, function(x) estimate_ve_uncond_logreg(x)),
+        clreg_ve = map_vec(linelist, function(x) estimate_ve_cond_logreg(x))
+    ) %>%
+    unnest(pars) %>%
+    mutate(mean_ratio = epsilon_v / epsilon_u)
+
+print("generating waning scenario plot")
+
+waning_insnt_vs_cumul_lm <- lm(insnt_ve ~ cumul_ve, data = final_ve_dt)
+waning_ulreg_vs_cumul_lm <- lm(ulreg_ve ~ cumul_ve, data = final_ve_dt)
+waning_clreg_vs_cumul_lm <- lm(clreg_ve ~ cumul_ve, data = final_ve_dt)
+
+waning_insnt_vs_cumul_adj_rsq <- summary(waning_insnt_vs_cumul_lm)$adj.r.squared
+waning_ulreg_vs_cumul_adj_rsq <- summary(waning_ulreg_vs_cumul_lm)$adj.r.squared
+waning_clreg_vs_cumul_adj_rsq <- summary(waning_clreg_vs_cumul_lm)$adj.r.squared
+
+waning_ve_compare <- final_ve_dt %>%
+    select(ends_with("_ve")) %>%
+    pivot_longer(!cumul_ve) %>%
+    group_by(name) %>%
+    summarize(mean_diff = mean(abs(value - cumul_ve))) %>%
+    ungroup() %>%
+    arrange(name) %>%
+    mutate(adj_rsq = c(waning_clreg_vs_cumul_adj_rsq,
+                       waning_insnt_vs_cumul_adj_rsq,
+                       waning_ulreg_vs_cumul_adj_rsq))
+
+waning_plt <- final_ve_dt %>%
+    pivot_longer(c(insnt_ve, ulreg_ve, clreg_ve)) %>%
+    ggplot() +
+        aes(x = cumul_ve, y = value, color = mean_ratio) +
+        geom_hline(yintercept = 0, linetype = "44", color = "gray50") +
+        geom_vline(xintercept = 0, linetype = "44", color = "gray50") +
+        geom_line(aes(y = cumul_ve), color = "black", linewidth = 1, linetype = "44") +
+        geom_point(size = 2) +
+        geom_smooth(
+            method = "lm",
+            linewidth = 2,
+            color = "#ff9f9faa"
+        ) +
+        geom_text(
+            data = waning_ve_compare,
+            aes(
+                x = -475,
+                y = 100,
+                label = paste0("Adj. r-squared: ", signif(adj_rsq, digits = 3))
+            ),
+            inherit.aes = FALSE
+        ) +
+        geom_text(
+            data = waning_ve_compare,
+            aes(
+                x = -475,
+                y = 85,
+                label = paste0("Mean abs. difference: ", signif(mean_diff, digits = 3))
+            ),
+            inherit.aes = FALSE
+        ) +
+        facet_grid(cols = vars(name)) +
+        scale_color_viridis_c(
+            name = "Mean pre-vaccintion susceptibility ratio",
+            limits = c(0.1, 10),
+            breaks = c(0.1, 10)
+        ) +
+        theme_cowplot(20) +
+        background_grid() +
+        theme(legend.position = "top") +
+        labs(x = "VE cumulative (%)", y = "Alternative VE estimate (%)")
+
+rm(pars_dt, final_ve_dt)
+
+# plot output
+fig_path <- here("plots", "supplemental_figs")
+dir.create(fig_path)
+
+ggsave(
+    here(fig_path, "ve_vs_mean_pre-vax_suscep_wo_waning.png"),
+    no_waning_plt,
+    bg = "white",
+    height = 9,
+    width = 12,
+    units = "in"
+)
+
+ggsave(
+    here(fig_path, "ve_vs_mean_pre-vax_suscep_w_waning.png"),
+    waning_plt,
+    bg = "white",
+    height = 9,
+    width = 12,
+    units = "in"
+)
