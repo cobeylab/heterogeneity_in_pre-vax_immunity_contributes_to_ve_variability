ca1f6b3c26c99e339bfdb3c10c5e06eebfb8163c
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2026-07-01 16:10:58 -0500

    renaming file and changing how ve estimates are compared
    now just evaluate the difference (not absolute difference)
    ealuate mean and 95% IQR of ve differences

--- scripts/supplemental_figures/ve_estimate_comparison.R
+++ scripts/supplemental_figures/ve_estimate_comparison.R
@@ -13,8 +13,7 @@ facet_labels = c(
     clreg_ve = "VE conditional"
 )
 
-### Final ve sweeping across infection hazard, mean pre-vaccination risk, and
-### true vaccine protection (NO WANING)
+### Final ve sweeping across mean pre-vaccination risk and true vaccine protection (NO WANING)
 
 set.seed(0)
 
@@ -23,8 +22,8 @@ pars = list(
     start_time = 0,
     end_time = 200,
     dt = 14,
-    lambda = c(0.0015, 0.005, 0.01),
-    lambda_negative = 0.02,
+    lambda = 0.005,
+    lambda_negative = 0.015,
     theta_0 = 1 - seq(0.1, 0.9, 0.1),
     eta = -1,
     epsilon_v = seq(0.1, 1.0, 0.1),
@@ -36,7 +35,7 @@ pars = list(
     pi_pos = 1,
     pi_neg = 1,
     c = 0.5,
-    N = 1e3
+    N = 5e3
 )
 
 pars$time <- pars$end_time
@@ -71,24 +70,28 @@ final_ve_dt <- pars_dt %>%
         cumul_ve = map_vec(pars, function(x) estimate_math_ve(x, cumul_ve_opts)),
         ulreg_ve = map_vec(linelist, function(x) estimate_ve_uncond_logreg(x)),
         clreg_ve = map_vec(linelist, function(x) estimate_ve_cond_logreg(x))
-    ) %>%
-    unnest(pars)
+    )
 
 # Calculate absolute average difference between VE estimates
 no_waning_ve_compare <- final_ve_dt %>%
     select(ends_with("_ve")) %>%
     pivot_longer(!cumul_ve) %>%
+    mutate(ve_diff = value - cumul_ve) %>%
     group_by(name) %>%
-    summarize(mean_diff = mean(abs(value - cumul_ve))) %>%
-    ungroup()
+    arrange(ve_diff) %>%
+    summarize(
+        mean_diff = mean(ve_diff, na.rm = TRUE),
+        lower_diff = quantile(ve_diff, probs = c(0.025), na.rm = TRUE),
+        upper_diff = quantile(ve_diff, probs = c(0.925), na.rm = TRUE)
+    )
 
-print("Average absolute difference between regression-based VE and VE from cumulative attack rates")
-print("Scenarios: varying infection hazard, true vax protection, pre-vax risk means")
+print("Mean and 95% IQR of difference between VE estimates and VE^cumulative")
+print("Scenarios: varying true vax protection and pre-vax risk means")
 print(no_waning_ve_compare)
 
 rm(pars_dt, final_ve_dt)
 
-### Final ve sweeping across infection hazard, mean pre-vaccination risk, and
+### Final ve sweeping across waning rate, mean pre-vaccination risk, and
 ### true vaccine protection (WITH WANING)
 
 set.seed(0)
@@ -98,8 +101,8 @@ pars = list(
     start_time = 0,
     end_time = 200,
     dt = 14,
-    lambda = c(0.0015, 0.005, 0.01),
-    lambda_negative = 0.02,
+    lambda = 0.0015,
+    lambda_negative = 0.0045,
     theta_0 = 1 - c(0.3, 0.6, 0.9),
     eta = c(30, 180, 360, 1440),
     epsilon_v = seq(0.1, 1.0, 0.1),
@@ -111,7 +114,7 @@ pars = list(
     pi_pos = 1,
     pi_neg = 1,
     c = 0.5,
-    N = 1e3
+    N = 5e3
 )
 
 pars$time <- pars$end_time
@@ -153,84 +156,15 @@ final_ve_dt <- pars_dt %>%
 waning_ve_compare <- final_ve_dt %>%
     select(ends_with("_ve")) %>%
     pivot_longer(!cumul_ve) %>%
+    mutate(ve_diff = value - cumul_ve) %>%
     group_by(name) %>%
-    summarize(mean_diff = mean(abs(value - cumul_ve))) %>%
-    ungroup()
+    arrange(ve_diff) %>%
+    summarize(
+        mean_diff = mean(ve_diff, na.rm = TRUE),
+        lower_diff = quantile(ve_diff, probs = c(0.025), na.rm = TRUE),
+        upper_diff = quantile(ve_diff, probs = c(0.925), na.rm = TRUE)
+    )
 
-print("Average absolute difference between regression-based VE and VE from cumulative attack rates")
-print("Scenarios: varying infection hazard, true vax protection, pre-vax risk means, vax waning")
+print("Mean and 95% IQR of difference between VE estimates and VE^cumulative")
+print("Scenarios: varying true vax protection, pre-vax risk means and vax waning")
 print(waning_ve_compare)
-
-rm(pars_dt, final_ve_dt)
-
-### Final ve sweeping across infection hazard, and pre-vaccination risk heterogeneity (NO WANING)
-
-set.seed(0)
-
-# Scenario parameters
-pars = list(
-    start_time = 0,
-    end_time = 200,
-    dt = 14,
-    lambda = c(0.0015, 0.005, 0.01),
-    lambda_negative = 0.0045,
-    theta_0 = 1 - seq(0.1, 0.9, 0.1),
-    eta = -1,
-    epsilon_v = 1.0,
-    epsilon_u = 1.0,
-    alpha_v = seq(0.2, 2, 0.2),
-    alpha_u = seq(0.2, 2, 0.2),
-    mu_v = 1,
-    mu_u = 1,
-    pi_pos = 1,
-    pi_neg = 1,
-    c = 0.5,
-    N = 1e3
-)
-
-pars$time <- pars$end_time
-
-# Scenario options (no waning, continuous pre-vaccination risk, VE from cumulative attack rates)
-opts = list(
-    waning = FALSE,
-    heterogeneity = TRUE,
-    instantaneous = FALSE
-)
-cumul_ve_opts <- opts
-
-# Create copy of options for estimating VE from instantaneous incidence rates
-insnt_ve_opts <- opts
-insnt_ve_opts$instantaneous <- TRUE
-
-# Generate parameter sets
-pars_dt <- crossing(!!!pars) %>%
-    mutate(par_set_id = row_number()) %>%
-    group_by(par_set_id) %>%
-    nest(pars = -group_cols()) %>%
-    ungroup()
-
-print(paste0("generating ", nrow(pars_dt)," heterogeneity scenario linelists"))
-
-# Generate VE data (analytical and logistic regression models)
-final_ve_dt <- pars_dt %>%
-    rowwise_mutate(counts = list(generate_counts(pars, opts))) %>%
-    mutate(linelist = map(counts, function(x) generate_linelist(x))) %>%
-    mutate(
-        insnt_ve = map_vec(pars, function(x) estimate_math_ve(x, insnt_ve_opts)),
-        cumul_ve = map_vec(pars, function(x) estimate_math_ve(x, cumul_ve_opts)),
-        ulreg_ve = map_vec(linelist, function(x) estimate_ve_uncond_logreg(x)),
-        clreg_ve = map_vec(linelist, function(x) estimate_ve_cond_logreg(x))
-    ) %>%
-    unnest(pars)
-
-# Calculate absolute average difference between VE estimates
-het_ve_compare <- final_ve_dt %>%
-    select(ends_with("_ve")) %>%
-    pivot_longer(!cumul_ve) %>%
-    group_by(name) %>%
-    summarize(mean_diff = mean(abs(value - cumul_ve))) %>%
-    ungroup()
-
-print("Average absolute difference between regression-based VE and VE from cumulative attack rates")
-print("Scenarios: varying infection hazard, true vax protection, pre-vax risk heterogeneities")
-print(het_ve_compare)
