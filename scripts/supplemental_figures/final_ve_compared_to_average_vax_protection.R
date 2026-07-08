e70344e5b69a3205de999833cf986fe0cc94f331
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2026-07-08 12:10:52 -0500

    adding lewnard fig to final ve vs. average protection plot

--- scripts/supplemental_figures/final_ve_compared_to_average_vax_protection.R
+++ scripts/supplemental_figures/final_ve_compared_to_average_vax_protection.R
@@ -18,6 +18,73 @@ opts = list(
     instantaneous = FALSE
 )
 
+# Lewnard plot scenario parameters
+pars <- list(
+    start_time = 200,
+    end_time = 200,
+    dt = 0,
+    lambda = c(0.001, 0.005, 0.01),
+    theta_0 = 1 - seq(0, 1, 0.01),
+    epsilon_v = 1,
+    epsilon_u = 1,
+    alpha_v = Inf,
+    alpha_u = Inf,
+    eta = Inf
+)
+
+# Generate lewnard plot data
+lewnard_dt <- generate_par_sets(pars, include_early = FALSE) %>%
+    mutate(par_set_id = row_number()) %>%
+    group_by(par_set_id) %>%
+    nest(pars = -group_cols()) %>%
+    ungroup() %>%
+    mutate(
+        estd_ve = map_vec(pars, function(x) estimate_math_ve(x, opts))
+    ) %>%
+    unnest(pars) %>%
+    mutate(
+        true_vax_protect = (1 - theta_0) * 100,
+        bias = estd_ve - true_vax_protect
+    )
+
+# Find true vaccine protection at which bias is highest when there is no waning
+intermediate_VE <- round(lewnard_dt$true_vax_protect[abs(lewnard_dt$bias) == max(abs(lewnard_dt$bias))])
+
+lewnard_plt <- ggplot(lewnard_dt) +
+    aes(x = true_vax_protect, y = estd_ve, color = factor(lambda)) +
+    geom_line(aes(y = true_vax_protect), color = "black", linetype = "44") +
+    geom_segment(
+        x = 58,
+        xend = 58,
+        y = 0,
+        yend = 58,
+        color = "maroon",
+        linetype = "33",
+        linewidth = 1
+    ) +
+    annotate(
+        "label",
+        x = 82.5,
+        y = 10,
+        label = "Largest distance between VE\nand true protection occurs at\n58% true vaccine protection",
+        color = "maroon",
+        size = 3
+    ) +
+    geom_line(linewidth = 1) +
+    scale_color_manual(
+        name = "Exogeneous infection hazard",
+        breaks = c(0.001, 0.005, 0.01),
+        values = c("dodgerblue", "darkorange", "darkorchid")
+    ) +
+    theme_cowplot(14) +
+    theme(
+        legend.position = "inside",
+        legend.position.inside = c(0.05, 0.845),
+        legend.box.background = element_rect(fill = "#ffffffaa", color = "#ffffff00")
+    ) +
+    background_grid() +
+    labs(x = "True vaccine protection (%)", y = "Final VE (%)")
+
 # Heatmap scenario parameters
 pars <- list(
     start_time = 200,
@@ -33,7 +100,7 @@ pars <- list(
 )
 
 # Generate VE data
-dt <- generate_par_sets(pars, include_early = FALSE) %>%
+heatmap_dt <- dt <- generate_par_sets(pars, include_early = FALSE) %>%
     mutate(par_set_id = row_number()) %>%
     group_by(par_set_id) %>%
     nest(pars = -group_cols()) %>%
@@ -48,14 +115,12 @@ dt <- generate_par_sets(pars, include_early = FALSE) %>%
                             labels = c(30, 60, 90, 180, 360, 720, 1440, "No\nwaning")),
         starting_vax_protect = (1 - theta_0) * 100,
         avg_vax_protect = (1 - (tot_vax_effect / time)) * 100,
-        bias = estd_ve - avg_vax_protect
+        bias = estd_ve - avg_vax_protect,
+        true_vax_protect = (1 - theta_0) * 100
     )
 
-# Find true vaccine protection at which bias is highest when there is no waning
-intermediate_VE <- round(dt$avg_vax_protect[abs(dt$bias) == max(abs(dt$bias))])
-
-# Panel A: heatmap of final VE bias across starting vaccine protection and waning rate
-bias_plt <- ggplot(dt) +
+# heatmap of final VE bias across starting vaccine protection and waning rate
+bias_plt <- ggplot(heatmap_dt) +
     aes(
         x = eta_f,
         y = factor(starting_vax_protect),
@@ -76,8 +141,8 @@ bias_plt <- ggplot(dt) +
         y = "Starting true vaccine protection (%)"
     )
 
-# Panel B: heatmap of average vaccine protection across starting protection and waning rate
-avg_eff_plt <- ggplot(dt) +
+# heatmap of average vaccine protection across starting protection and waning rate
+avg_eff_plt <- ggplot(heatmap_dt) +
     aes(
         x = eta_f,
         y = factor(starting_vax_protect),
@@ -103,7 +168,7 @@ avg_eff_plt <- ggplot(dt) +
 
 # Combine panels and save
 plt <- plot_grid(
-  bias_plt, avg_eff_plt,
+  lewnard_plt, bias_plt, avg_eff_plt,
   nrow = 1,
   labels = "AUTO"
 )
@@ -114,7 +179,7 @@ dir.create(fig_dir)
 ggsave(
     here(fig_dir, "final_ve_compared_to_avg_vax_protect.png"),
     plt,
-    width = 10,
+    width = 15,
     height = 5,
     units = "in",
     bg = "white"
@@ -123,7 +188,7 @@ ggsave(
 ggsave(
     here(fig_dir, "final_ve_compared_to_avg_vax_protect.pdf"),
     plt,
-    width = 10,
+    width = 15,
     height = 5,
     units = "in",
     bg = "white"
