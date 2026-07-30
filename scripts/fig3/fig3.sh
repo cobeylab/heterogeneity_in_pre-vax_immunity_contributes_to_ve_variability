69d18bbf733d1860d1624474316c848b94d85988
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2026-07-30 14:31:22 -0500

    simplifying fig3 generation script

--- scripts/fig3/fig3.sh
+++ scripts/fig3/fig3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+config=$1
+
 echo "RUNNING SIMULATION"
-julia run_model.jl --config fig3.toml
+julia run_model.jl --config $1
 
 echo "COLLECTING RESULTS"
-julia collect_results.jl --config fig3.toml
+julia collect_results.jl --config $1
 
 echo "GENERATING FIGURE"
-Rscript fig3.R --config fig3.toml
+Rscript fig3.R --config $1
