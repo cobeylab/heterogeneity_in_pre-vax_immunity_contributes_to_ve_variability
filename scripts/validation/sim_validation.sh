cefa12a2c03d43377a413c607454e97855f3dc87
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2026-08-26 09:17:21 -0500

    bash scripts now stop on an error

--- scripts/validation/sim_validation.sh
+++ scripts/validation/sim_validation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 echo "RUNNING SIMULATION"
 julia ../fig3/run_model.jl --config config.toml
 
 echo "COLLECTING RESULTS"
-julia ../fig3/collect_results.jl --config config.toml
+julia ../fig3/collect_results.jl --config config.toml --clean true
 
 echo "GENERATING FIGURE"
 Rscript sim_validation.R --config config.toml
